e8f4d6eea311f3090e9420984ac986052436b7d7
Adrianchin <98614997+Adrianchin@users.noreply.github.com>
2022-04-07 15:36:17 -0600

    Working email and credential check

--- server.js
+++ server.js
@@ -1,11 +1,36 @@
 const express = require('express');
 const cors = require ('cors');
 const app = express();
+const {MongoClient} = require("mongodb");
 
 const TextTranslation = require("./components/TextTranslation")
 const Tokenizer = require("./components/Tokenizer")
 const ImageUpload = require("./components/imageupload/ImageUpload")
-const LinkUpload = require("./components/linkupload/LinkUpload")
+const LinkUpload = require("./components/linkupload/LinkUpload");
+const { response } = require('express');
+
+const uri = "mongodb+srv://Adrian:Adrian1993@cluster0.jajtv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri);
+
+/*
+async function testMongo(){
+    try{
+    await client.connect();
+    await listDatabases(client);
+    } catch (error) {
+        console.log(error);
+    }finally {
+        await client.close();
+    }
+}
+testMongo().catch(console.error)
+
+async function listDatabases(client){
+    databasesList = await client.db().admin().listDatabases();
+    console.log("Databases:");
+    databasesList.databases.forEach(db => console.log(` - ${db.name}`));
+};
+*/
 
 app.use(cors());
 app.use(express.json());
@@ -74,6 +99,63 @@ app.post("/signin", (req, res) => {
         };
     }
     return res.status(400).json('wrong credentials')
+})
+
+app.post("/register", (req, res) => {
+    async function testMongo(){
+        try{
+            await client.connect();
+            const loginSubmission = req.body;
+            const userProfile = {
+                username: loginSubmission.username,
+                email: loginSubmission.email,
+                userData: null
+            }
+            const userCredentials = {
+                username: loginSubmission.username.toLowerCase(),
+                email: loginSubmission.email.toLowerCase(),
+                password: loginSubmission.password
+            }
+            //Tests for if username or email exists
+            let testForUsername = await searchForUsername(client, loginSubmission.username.toLowerCase());
+            let testForEmail = await searchForEmail(client, loginSubmission.email.toLowerCase());
+
+            if(!loginSubmission.username || !loginSubmission.email || !loginSubmission.password ){
+                console.log("test empty")
+                return res.status(400).json('incorrect form submission')
+            }else if(testForUsername != null){
+                console.log("test Username")
+                return res.status(400).json('Username already exists')
+            }else if(testForEmail != null){
+                console.log("test Email")
+                return res.status(400).json('Email already exists')
+            }else {
+                await createUserProfile(client, userProfile, userCredentials);
+                res.json(userProfile)
+                } 
+        }catch (error) {
+            console.log(error);
+        }finally {
+            await client.close();
+        }
+    }
+    testMongo().catch(console.error)
+
+    async function createUserProfile(client, userProfile, userCredentials){
+        await client.db("profile_information").collection("user_profile").insertOne(userProfile);
+        await client.db("profile_information").collection("user_login_data").insertOne(userCredentials);
+    };
+
+    async function searchForUsername(client, username){
+        const resultSearchForUsername = await client.db("profile_information").collection("user_login_data").findOne({ username: username});
+        return resultSearchForUsername;
+    };
+
+    async function searchForEmail(client, email){
+        const resultSearchForEmail = await client.db("profile_information").collection("user_login_data").findOne({ email: email});
+        return resultSearchForEmail;
+    };
+
 })
 
 //NOT USED RIGHT NOW
