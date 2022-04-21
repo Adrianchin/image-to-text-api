067c58e5b3f255afea0ee762ed3cdff548331387
Adrianchin <98614997+Adrianchin@users.noreply.github.com>
2022-04-21 11:14:12 -0600

    Removed old code

--- passportconfig/Passport.js
+++ passportconfig/Passport.js
@@ -2,10 +2,6 @@ const passport = require("passport");
 const LocalStratagy = require("passport-local").Strategy;
 const {validPassword} = require("../components/authutility/AuthenticationTools")
 
-//const {MongoClient, ObjectId} = require("mongodb");
-//const uri = "mongodb+srv://Adrian:Adrian1993@cluster0.jajtv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-//const client = new MongoClient(uri);
-
 const {UserLoginData} = require ("../db/Models")
 
 const customFields = {
@@ -30,26 +26,6 @@ async function verifyCallback(username, password, done) {
     }catch (err){
         done(err)
     }
-    /*
-    let userCredentials;
-    try{
-        await client.connect();
-        userCredentials = await client.db("profile_information").collection("user_login_data").findOne({ username: username});
-    }catch(error){
-        console.log("Problem with client connect to DB")
-        return done(null,false)
-    }finally {
-        await client.close();
-    }
-    if (!userCredentials){return done(null,false)}
-
-    const isValid = validPassword(password, userCredentials.hash, userCredentials.salt);
-
-    if (isValid) {
-        return done(null, userCredentials);
-    }else{
-        return done(null, false);
-    }*/
 }
 
 const stratagy = new LocalStratagy(customFields, verifyCallback);
@@ -67,66 +43,3 @@ passport.deserializeUser((id, done) => {
     })
     .catch(err => done(err))
 });
-    /*let userCredentials;
-    try{
-        await client.connect();
-        userCredentials = await client.db("profile_information").collection("user_login_data").findOne({ _id: userId});
-        if(userCredentials){
-            return done(null, userCredentials)
-        } else {
-            return done(null, false);
-        }
-    }catch(error){
-        console.log("Problem with client connect to DB")
-        return done(null,false)
-    }finally {
-        await client.close();
-    }
-});*/
-
-/*
-const passport = require("passport");
-const LocalStratagy = require("passport-local").Strategy;
-const connection = require("./database");
-const User = connection.models.User
-
-const customFields = {
-    usernameField: "uname",
-    passwordField: "pw"
-};
-/*
-const verifyCallback = (username, password, done) = {
-    User.findOne({username:username})
-    .then((user) => {
-        if (!user){return done(null,false)}
-        //function defined at bottom of app js
-        const isValid = validPassword(password, user.hash, user.salt);
-
-        if (isValid) {
-            return done(null, user);
-        }else{
-            return done(null, false);
-        }
-    })
-    .catch((err)=>{
-        done(err);
-    })
-}
-
-const stratagy = new LocalStratagy(customFields, verifyCallback);
-
-passport.use(stratagy);
-
-passport.serializeUser((user, done) => {
-    done(null, user.id);
-});
-
-passport.deserializeUser((userId, done) => {
-    User.findById(userId)
-    .then((user)=>{
-        done(null, user);
-    })
-    .catch(err => done(err))
-});
-
-*/
--- server.js
+++ server.js
@@ -137,25 +137,6 @@ app.put("/postdata", isAuth, (req, res) => {
         }
     }
     dataForUploadMongo();
-
-        /*
-        async function dataForUploadMongo(){
-        try{
-            await client.connect();
-            const dataForUpload=req.body;
-            await uploadDataToMongo(client, dataForUpload);
-            return res.json("Successfully Uploaded to DB: ");
-        }catch (error) {
-            console.log(error);
-        }finally {
-            await client.close();
-        }
-    }
-    dataForUploadMongo();
-
-    async function uploadDataToMongo(client, dataForUpload){
-        await client.db("profile_information").collection("app_data").insertOne(dataForUpload);
-    };*/
 })
 
 app.post("/deletedocument", isAuth, (req, res) => {
@@ -169,27 +150,6 @@ app.post("/deletedocument", isAuth, (req, res) => {
         }
     }
     deleteDocument()
-    
-    /*
-    async function deleteDocumentMongo(){
-        try{
-            await client.connect();
-            const documentForDelete=new ObjectId(req.body._id);
-            //console.log(documentForDelete);
-            const returnDocumentDelete = await deleteDocument(client, documentForDelete);
-            return res.json(returnDocumentDelete);
-        }catch (error) {
-            console.log(error);
-        }finally {
-            await client.close();
-        }
-    }
-    deleteDocumentMongo();
-
-    async function deleteDocument(client, documentForDelete){
-        const deleteDocumentResult =await client.db("profile_information").collection("app_data").deleteOne({_id: documentForDelete});
-        return deleteDocumentResult;
-    };*/
 })
 
 app.post("/signin", passport.authenticate('local'), function(req, res) {
@@ -199,47 +159,11 @@ app.post("/signin", passport.authenticate('local'), function(req, res) {
             let returnedUserInformation = await UserLoginData.findOne({loweCaseUsername:loginSubmission.username.toLowerCase(),}).select({username:1,email:1}).lean();
             let placeholder = await findProfileDataById(returnedUserInformation._id);
             returnedUserInformation["profile"]=placeholder;
-            //console.log("pass 2 ", placeholder)
-            //console.log(typeof returnedUserInformation)
-            //console.log(typeof placeholder)
             return res.json(returnedUserInformation);
         }catch (error) {
             console.log(error);
         }
     }loginStart()
-    /*
-    async function loginMongo(){
-        try{
-            await client.connect();
-            const loginSubmission = req.body;
-            let userCredentials = await searchForUsernameCredentials(client, loginSubmission.username.toLowerCase());
-            if(userCredentials.password === loginSubmission.password){
-                const returnedUserInformation = await searchForUsernameProfile(client, userCredentials.id)
-                returnedUserInformation["profile"] = await searchForUserData(client, String(returnedUserInformation._id))
-                return res.json(returnedUserInformation);
-            }else{
-                return res.status(400).json('wrong credentials')
-            }
-        }catch (error) {
-            console.log(error);
-        }finally {
-            await client.close();
-        }
-    }
-    loginMongo()
-    async function searchForUsernameCredentials(client, username){
-        const resultSearchForUsername = await client.db("profile_information").collection("user_login_data").findOne({ username: username});
-        return resultSearchForUsername;
-    };
-    async function searchForUsernameProfile(client, id){
-        const resultSearchForUsername = await client.db("profile_information").collection("user_profile").findOne({ _id: id});
-        return resultSearchForUsername;
-    };
-    async function searchForUserData(client, userID){
-        const resultSearchForUsername = await client.db("profile_information").collection("app_data").find({id: userID}).sort({ date: -1 }).toArray();
-        //console.log(resultSearchForUsername)
-        return resultSearchForUsername;
-    }*/
 })        
 
 app.get("/getProfileData", isAuth, (req,res) => {
@@ -253,27 +177,6 @@ app.get("/getProfileData", isAuth, (req,res) => {
         }
     }
     fetchProfileData();
-    /*
-    async function fetchProfileData(){
-        try{
-            await client.connect();
-            const userID = req.query.id;
-            //console.log("This is userID: ", userID )
-            let profileCardData = await searchForUserData(client, String(userID));
-            return res.json(profileCardData);
-        }catch (error) {
-            console.log(error);
-        }finally {
-            await client.close();
-        }
-    }
-    fetchProfileData();
-
-    async function searchForUserData(client, userID){
-        const resultSearchForUsername = await client.db("profile_information").collection("app_data").find({id: userID}).sort({ date: -1 }).toArray();
-        console.log(resultSearchForUsername)
-        return resultSearchForUsername;
-    }*/
 })
 
 app.post("/updatehistory", isAuth, (req, res) => {
@@ -290,29 +193,6 @@ app.post("/updatehistory", isAuth, (req, res) => {
         }
     }
     updateHistory()
-/*
-    async function updateHistory(){
-        try{ 
-            await client.connect();
-            //console.log(req.body)
-            const nameOfDocument = new ObjectId(req.body._id);
-            const translatedText = req.body.translatedText;
-            const tokenizedText = req.body.tokenizedText;
-            const date = req.body.date;
-            let response = await updateMongoHistory(client, nameOfDocument, {translatedText: translatedText, tokenizedText: tokenizedText, date: date});
-            return res.json(response);
-        }catch (error) {
-            console.log(error);
-        }finally {
-            await client.close();
-        }
-    }
-    updateHistory().catch(console.error)
-
-    async function updateMongoHistory(client, nameOfDocument, dataForUpdate){
-        const result = await client.db("profile_information").collection("app_data").updateOne({ _id: nameOfDocument }, { $set: dataForUpdate })
-        return result;
-    }*/
 })
 
 app.post("/register", (req, res) => {
@@ -352,63 +232,6 @@ app.post("/register", (req, res) => {
         }
     }
     registerUser()
-    /*
-    async function testMongo(){
-        try{
-            await client.connect();
-            const loginSubmission = req.body;
-
-            //Tests for if username or email exists
-            let testForUsername = await searchForUsername(client, loginSubmission.username.toLowerCase());
-            let testForEmail = await searchForEmail(client, loginSubmission.email.toLowerCase());
-
-            if(!loginSubmission.username || !loginSubmission.email || !loginSubmission.password ){
-                console.log("test empty")
-                return res.status(400).json('incorrect form submission')
-            }else if(testForUsername != null){
-                console.log("test Username")
-                return res.status(400).json('Username already exists')
-            }else if(testForEmail != null){
-                console.log("test Email")
-                return res.status(400).json('Email already exists')
-            }else {
-                await createUserProfile(client, loginSubmission);
-                } 
-        }catch (error) {
-            console.log(error);
-        }finally {
-            await client.close();
-        }
-    }
-    testMongo().catch(console.error)
-
-    async function createUserProfile(client, loginSubmission){
-        const userProfile = {
-            username: loginSubmission.username,
-            email: loginSubmission.email,
-            history: []
-        }
-        let result = await client.db("profile_information").collection("user_profile").insertOne(userProfile);
-        const userCredentials = {
-            username: loginSubmission.username.toLowerCase(),
-            email: loginSubmission.email.toLowerCase(),
-            id: result.insertedId,
-            password: loginSubmission.password
-        }
-        await client.db("profile_information").collection("user_login_data").insertOne(userCredentials);
-        res.json(userProfile);
-    };
-
-    async function searchForUsername(client, username){
-        const resultSearchForUsername = await client.db("profile_information").collection("user_login_data").findOne({ username: username});
-        return resultSearchForUsername;
-    };
-
-    async function searchForEmail(client, email){
-        const resultSearchForEmail = await client.db("profile_information").collection("user_login_data").findOne({ email: email});
-        return resultSearchForEmail;
-    };*/
-
 })
 
 //NOT USED RIGHT NOW
