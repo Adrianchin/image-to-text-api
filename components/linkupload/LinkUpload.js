169be19b5c3fc7417c2e8c75eb2d0ba60e840e8d
Adrianchin <98614997+Adrianchin@users.noreply.github.com>
2022-04-22 15:49:07 -0600

    Added test route 2, still to test

--- components/linkupload/LinkUpload.js
+++ components/linkupload/LinkUpload.js
@@ -29,6 +29,162 @@ console.log("Req body is", req.body);
         console.log(error);
     }
 }
+//Route for file upload route - IT WORKS
+const {
+    createApp_Data, 
+} = require("../../db/Models");
+const { request } = require('http');
+const { url } = require('inspector');
+
+async function linkFilesRoute(req, res, next) {
+
+    const requestData = {
+        linkImagePath: true,//
+        uploadImagePath: false,//
+        originalImageSize: null,//
+        imageInformation: null,//
+        imageURL: null,//
+        rawImageBox: null,//
+        translatedText: null,
+        tokenizedText: null,
+        date: new Date(),//
+        imageFileName: null,//
+      };
+
+      requestData.imageURL = req.body.link;
+
+    //Test for all calculations in 1 call
+    async function imagelinkphoto() {  
+        // Specifies the location of the api endpoint
+        const clientOptions = {apiEndpoint: 'eu-vision.googleapis.com'};
+        // Creates a client
+        const client = new vision.ImageAnnotatorClient(clientOptions);
+        // Performs text detection on the image file
+        try{
+            const [result] = await client.textDetection(`${requestData.imageURL}`);
+            requestData.imageInformation = result.textAnnotations;
+        } catch(error) {
+            res.status(400).json(`problem with the Google API`);
+            console.log(error);
+        }
+    }
+    await imagelinkphoto()
+
+    async function imageDimensions() {
+        //IMPORTANT: onload is async, so you need to put shit in it and whatever you are
+        //doing with it in the onload function!
+        //Note. I made a promise beause otherwise, onload will be async and we need the outputs
+        //for the box calculations. This is a general onload promise, which we then use for the image
+        function onLoadPromiseImageFunction(image) {
+          return new Promise((resolve, reject) => {
+            image.onload = () => resolve(image);
+            image.onerror = reject;
+          });
+        }
+  
+        let img = new Image();
+        let imgpromise = onLoadPromiseImageFunction(img);
+        img.src = requestData.imageURL;
+        await imgpromise;
+  
+        const originalHeight = img.height;
+        const originalWidth = img.width;
+  
+        const originalImageSize = {
+          height: originalHeight,
+          width: originalWidth,
+          type: "url",
+        };
+  
+        requestData.originalImageSize = originalImageSize; //For MongoDB
+  
+        //Note: Google API is 0,1,2,3, counterclockwise top left, 0,0 is top left. I have to take away the Original height from the bottom value
+        //Updates the box for render
+        requestData.rawImageBox = {
+          top: requestData.imageInformation[0].boundingPoly.vertices[0].y,
+          right: requestData.imageInformation[0].boundingPoly.vertices[1].x,
+          left: requestData.imageInformation[0].boundingPoly.vertices[0].x,
+          bottom: originalHeight - requestData.imageInformation[0].boundingPoly.vertices[2].y,
+        };
+      }
+    await imageDimensions(); //Step 2, Requires step 1
+
+        //Make this into a function from translate text
+    async function textForTranslation(){
+        try{
+            //REMOVE API KEY later!!!
+            const response = await fetch('https://api-free.deepl.com/v2/translate', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded',
+                    'Accept': '*/*'
+                },
+                body: new URLSearchParams({
+                    target_lang: 'EN',
+                    auth_key: 'ddec143e-2630-2a52-13fc-191f9cd1a070:fx',
+                    text: requestData.imageInformation[0].description
+                })
+            })
+            let textFromDeepL = await response.json();
+            //console.log("This is the text returned from DeepL", textFromDeepL);
+            requestData.translatedText = textFromDeepL.translations[0].text;
+        } catch(error){
+            res.status(400).json(`problem with the DeepL API`);
+            console.log(error);
+        }
+    }
+    await textForTranslation()
+
+    async function tokenizeText() {
+        let text=JSON.stringify({
+            text: requestData.imageInformation[0].description
+        });
+        try{
+            const response = await fetch('http://localhost:8010/japanesetoken', {
+                method: 'POST',
+                headers: {'Content-Type': 'application/json'},
+                body: text
+            })
+            let tokenizedResponse = await response.json();
+            //console.log("This is the text returned from tokenizer", tokenizedResponse);
+            requestData.tokenizedText = tokenizedResponse;
+        } catch(error){
+            res.status(400).json(`problem with the Tokenizer API`);
+            console.log(error);
+        }
+    }
+    await tokenizeText()
+    /*
+    async function dataForUploadMongo(){
+        try{
+            const result = await createApp_Data({
+                uploadImagePath:requestData.uploadImagePath,
+                originalImageSize:requestData.originalImageSize,
+                imageInformation:requestData.imageInformation,
+                imageURL:requestData.imageURL,
+                rawImageBox:requestData.rawImageBox,
+                translatedText:requestData.translatedText,
+                tokenizedText:requestData.tokenizedText,
+                id:req.session.passport.user.id, //taken from cookie
+                username:req.session.passport.user.username, //taken from cookie
+                linkImagePath:requestData.linkImagePath,
+                date:requestData.date,
+                imageFileName:requestData.imageFileName,
+            })
+            //console.log(result)
+        }catch(error) {
+            console.log("Error in posting: ", error);
+            return res.status(500).send("Problem uploading data" , req.body);
+        }
+    }
+    await dataForUploadMongo();
+    */
+    return res.json(requestData);
+}
+
+
+
+
 
 //For google API call - Photos Local (saved on device) - Not used
 async function localImagePhoto(req,res) {
